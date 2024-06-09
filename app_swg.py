2b0e73f79969bc8faa2fff96c41709fd1f6ed084
Anderson J. Santos <anderjsan@gmail.com>
2024-06-09 08:10:39 -0300

    [RELEASE] Dind-Jenkins Clean Code

--- app_swg.py
+++ app_swg.py
@@ -2,6 +2,13 @@ from flask import Flask
 from updater.updater import Updater
 from updater.app_swagger import AppSwagger
 
+def initialize_app():
+    app = Flask(__name__)
+    updater = Updater(app)
+    swg = AppSwagger(app)
+
+    updater.run()
+
 if __name__ == "__main__":
     app = Flask(__name__)
     updater = Updater(app)
